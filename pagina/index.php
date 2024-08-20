27f4ff94729a7e2ee0f3f5f88622105324928152
Carlos <carlosbalsamini@gmail.com>
2024-08-20 13:42:01 -0300

    Pequeños ajustes :)

--- pagina/index.php
+++ pagina/index.php
@@ -1,4 +1,128 @@
-<?php include "./components/header.html"?>
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Bootstrap 5.3.3 -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+      crossorigin="anonymous"
+    />
+    <!-- Horizon font -->
+    <link href="https://fonts.cdnfonts.com/css/horizon" rel="stylesheet" />
+    <!-- Codec Pro font-->
+    <link href="https://fonts.cdnfonts.com/css/codec-pro" rel="stylesheet">
+    <!-- CSS Links-->
+    <link rel="stylesheet" href="index_css/menu.css">
+    <link rel="stylesheet" href="index_css/header.css" />
+    <link rel="stylesheet" href="index_css/style.css">
+
+    <title>Inicio</title>
+  </head>
+  <body id="body" style="max-width: 100vW">
+    <!-- Header -->
+    <header
+      class="d-flex justify-content-center align-items-center flex-column p-0 position-fixed flex-md-row"
+    >
+    <!-- Mobile -->
+      <div class="d-flex justify-content-between align-items-center w-100 d-md-none">
+        <a href="index.php"><img src="imgs/icon.png" alt="icon.png" /></a>
+        <img src="imgs/horizonsports.png" alt="horizonsport.png">
+        <img src="imgs/menu.png" alt="menu.png" onclick="desplegar()" />
+      </div>
+    <!-- End Mobile -->
+    <!-- Dekstop-->
+      <div class="d-none justify-content-center align-items-center d-md-flex sm-horizon">
+        <a href="index.php"><img src="imgs/desktop/Desktop.png" alt="icon.png" class="sm-icon"/></a>
+      </div>
+    <!-- End Dekstop-->
+      <form
+        action="#"
+        method="GET"
+        class="d-flex justify-content-center align-items-center flex-row mb-3 p-0 rounded-1"
+      >
+        <input
+          type="search"
+          name="buscar"
+          placeholder="Buscar productos..."
+          class="ps-2 m-0"
+        />
+        <button class="rounded-5">
+          <img src="imgs/lupa.png" alt="lupa.png" class="p-0 m-0" />
+        </button>
+      </form>
+      <!-- Dekstop Menu-->
+       <div class="d-none d-md-flex dekstop-menu">
+        <p onclick="desplegarDesktop()"><img src="imgs/flecha-derecha.png" alt="flecha">Tienda</p>
+        <div id="productos-sm">
+          <ul class="productos-sm d-none d-md-flex flex-column justify-content-center align-items-center p-3 gap-3">
+            <li><a href="tienda.html">Ropa Deportiva</a></li>
+            <li><a href="tienda.html">Calzado Deportivo</a></li>
+            <li><a href="tienda.html">Equipamiento Deportivo</a></li>
+            <li><a href="tienda.html">Nutricion y Suplementos</a></li>
+            <li><a href="tienda.html">Fitness y Entrenamiento</a></li>
+          </ul>
+      </div>
+        <div class="d-flex user-menu-desktop">
+          <ul class="d-flex justify-content-center align-items-center">
+            <li><a href="carrito.php"><img src="imgs/carrito-de-compras.png" alt="carrito.png"></a></li>
+            <li><a href="#"><img src="imgs/guardado.png" alt="guardado.png"></a></li>
+            <li onclick="desplegarPerfil()"><img src="imgs/user.png" alt="inicio.png"></li>
+        </ul>
+        </div>
+        <div id="perfil-desktop" class="links-perfil justify-content-center align-items-center flex-row">
+            <a href="session/inicio_sesion.html">Iniciar Sesion</a><a href="session/registro.html">Registrarse</a>
+        </div>
+       </div>
+      <!-- End Dekstop Menu-->
+    </header>
+    <!-- End Header -->
+    <!-- Menu Screen -->
+    <div id="menu_screen"></div>
+
+    <!-- Menu -->
+    <div id="menu" class="menu d-flex align-items-center flex-column text-white position-fixed h-100">
+    <img src="imgs/x.png" alt="x.png" onclick="desplegar()">
+      <div class="d-flex flex-column w-100">
+        <div class="d-flex flex-row m-4 mb-3">
+          <img src="imgs/user.png" alt="user.png" class="bg-light" />
+          <h3 class="ms-3 mt-3 text-center fw-bold">Bienvenido</h3>
+        </div>
+        <p class="text-center ps-3 pe-3">Ingresa a tu cuenta para ver tus datos, compras y mas.</p>
+        <div class="links d-flex justify-content-center align-items-center flex-row gap-3">
+            <a href="session/inicio_sesion.html">Iniciar Sesion</a><p>|</p><a href="session/registro.html">Registrarse</a>
+        </div>
+      </div>
+
+      <hr>
+
+        <h3 class="text-center fw-bold ">Productos</h3>
+        <ul class="d-flex flex-column gap-3 w-100 mt-2">
+            <li><a href="tienda.html">Ropa Deportiva<img src="imgs/flecha-derecha.png" alt="flecha"></a></li>
+            <li><a href="tienda.html">Calzado Deportivo<img src="imgs/flecha-derecha.png" alt="flecha"></a></li>
+            <li><a href="tienda.html">Equipamiento Deportivo<img src="imgs/flecha-derecha.png" alt="flecha"></a></li>
+            <li><a href="tienda.html">Nutricion y Suplementos<img src="imgs/flecha-derecha.png" alt="flecha"></a></li>
+            <li><a href="tienda.html">Fitness y Entrenamiento<img src="imgs/flecha-derecha.png" alt="flecha"></a></li>
+        </ul>
+
+       <hr>
+
+       <ul class="user_menu row d-flex gap-3 mt-2">
+            <li><a href="index.php"><img src="imgs/casa.png" alt="inicio.png">Inicio</a></li>
+            <li><a href="carrito.php"><img src="imgs/carrito-de-compras.png" alt="carrito.png">Carrito de compras</a></a></li>
+            <li><a href="#"><img src="imgs/guardado.png" alt="guardado.png">Guardados</a></a></li>
+            <li><a href="#"><img src="imgs/terminos-y-condiciones.png" alt="terminos.png">Terminos y Condiciones</a></a></li>
+            <li><a href="#"><img src="imgs/informacion.png" alt="faq.png">Preguntas frecuentes</a></a></li>
+        </ul>
+
+        <hr>
+
+        <div class="footer_menu text-center w-100 m-1">
+            © 2024 Todos los derechos reservados.
+        </div>
+    </div>
 
     <div class="primerbloque">
       <img src="imgs/primerbloque.png" alt="primerbloque.png">
